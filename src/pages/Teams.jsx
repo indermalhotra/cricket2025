317809421e22566bf0404a40a2c7b8f2478de1c8
indermalhotra <indermalhotra@gmail.com>
2025-11-16 14:54:39 +0530

    Updated winner useeffect shifted all to seperate function

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -4,7 +4,7 @@ import TeamContext from "../context/TeamContext";
 import { useNavigate } from "react-router-dom";
 import PlayersCard from "../components/PlayersCard";
 import Button from "../components/Button";
-import { ballsToOvers } from "../utils/functions";
+import { ballsToOvers, endFirstInning, decideBattingBowlingTeams, endSecondInning, randomScoreArr } from "../utils/functions";
 
 const TeamContainer = styled.div`
   display: flex;
@@ -95,214 +95,7 @@ function Teams() {
     dispatch({ type: "SET_DECIDE", payload: call });
   }
 
-  const outcomes = [
-    4,
-    2,
-    2,
-    0,
-    4,
-    "Bowled",
-    1,
-    "Wide",
-    6,
-    1,
-    1,
-    2,
-    6,
-    0,
-    3,
-    1,
-    6,
-    "NB",
-    0,
-    1,
-    4,
-    4,
-    1,
-    1,
-    "LBW",
-    2,
-    4,
-    "Bowled",
-    2,
-    4,
-    3,
-    1,
-    0,
-    "Wide",
-    2,
-    2,
-    1,
-    4,
-    0,
-    4,
-    2,
-    "NB",
-    1,
-    6,
-    6,
-    3,
-    1,
-    0,
-    6,
-    2,
-    1,
-    4,
-    2,
-    "NB",
-    6,
-    6,
-    4,
-    1,
-    1,
-    4,
-    3,
-    0,
-    2,
-    1,
-    "Wide",
-    4,
-    6,
-    0,
-    0,
-    6,
-    2,
-    2,
-    0,
-    1,
-    2,
-    "NB",
-    4,
-    4,
-    4,
-    6,
-    2,
-    3,
-    "Wide",
-    0,
-    6,
-    6,
-    4,
-    4,
-    "LBW",
-    1,
-    4,
-    2,
-    "Bowled",
-    6,
-    1,
-    "Wide",
-    "NB",
-    2,
-    1,
-    0,
-    3,
-    4,
-    6,
-    0,
-    "Bowled",
-    2,
-    1,
-    0,
-    2,
-    4,
-    "Wide",
-    1,
-    "NB",
-    4,
-    3,
-    6,
-    0,
-    1,
-    6,
-    0,
-    2,
-    0,
-    1,
-    2,
-    "Wide",
-    2,
-    3,
-    "NB",
-    6,
-    0,
-    4,
-    2,
-    1,
-    1,
-    0,
-    0,
-    2,
-    "Bowled",
-    1,
-    2,
-    4,
-    3,
-    "Wide",
-    "NB",
-    1,
-    0,
-    2,
-    6,
-    1,
-    2,
-    6,
-    1,
-    6,
-    0,
-    3,
-    "Wide",
-    "NB",
-    1,
-    2,
-    4,
-    6,
-    0,
-    2,
-    4,
-    3,
-    6,
-    1,
-    2,
-    "Wide",
-    "NB",
-    "LBW",
-    3,
-    4,
-    0,
-    6,
-    1,
-    6,
-    2,
-    4,
-    0,
-    1,
-    "Wide",
-    "NB",
-    3,
-    4,
-    2,
-    6,
-    6,
-    2,
-    "Bowled",
-    1,
-    0,
-    4,
-    "Wide",
-    "NB",
-    2,
-    4,
-    1,
-    0,
-    3,
-    6,
-    4,
-    6,
-    2,
-    "LBW",
-    1,
-  ];
+  const outcomes = randomScoreArr();
 
   function getRandomOutcome() {
     const randomIndex = Math.floor(Math.random() * outcomes.length);
@@ -325,58 +118,15 @@ function Teams() {
     if (state.winner) return;
 
     if (state[selectTeam]?.balls) {
-      let battingTeam;
-      let battingKey;
-      let ballingTeam;
-      let ballingKey;
-
-      if (state.batting === state.team1.name) {
-        battingTeam = state.team1.name;
-        battingKey = "team1";
-        ballingTeam = state.team2.name;
-        ballingKey = "team2";
-      } else {
-        battingTeam = state.team2.name;
-        battingKey = "team2";
-        ballingTeam = state.team1.name;
-        ballingKey = "team1";
-      }
-      console.log("Ekumcheck team batting", battingTeam, battingKey);
-      console.log("Ekumcheck team batting", ballingTeam, ballingKey);
+      const [battingKey, ballingTeam, ballingKey] = decideBattingBowlingTeams(state);
 
       if (state.inning === 1) {
         console.log("Ekumcheck Its Inning 1");
-        if (
-          state[battingKey].balls < state.overs * 6 &&
-          state[battingKey].wicket < 11
-        ) {
-          console.log("Ekumcheck Its Inning 1 Continue");
-        } else {
-          console.log("Ekumcheck Its Inning 1 End");
-          dispatch({
-            type: "SET_INNINGS_END",
-            payload: {
-              inn: 2,
-              team: ballingTeam,
-              target: state[battingKey].score + 1,
-            },
-          });
-        }
+        endFirstInning(state, battingKey, ballingTeam, dispatch);
+        
       } else {
-        if (state[battingKey].score <= state.target) {
-          console.log("Ekumcheck 1", state.target, state[battingKey].score)
-          if (
-            state[battingKey].balls < state.overs * 6 &&
-            state[battingKey].wicket < 11
-          ) {
-            console.log("All Good 2nd inning can conitinue");
-          } else {
-            dispatch({ type: "SET_WINNER", payload: state[ballingKey] });
-          }
-        } else {
-          console.log("Ekumcheck 2", state.target, state[battingKey].score)
-          dispatch({ type: "SET_WINNER", payload: state[battingKey] });
-        }
+        console.log("Ekumcheck Its Inning 2");
+        endSecondInning(state, battingKey, ballingKey, dispatch)
       }
     }
   }, [selectTeam, state, dispatch]);
--- src/utils/functions.jsx
+++ src/utils/functions.jsx
@@ -9,48 +9,116 @@ export function getComputerTeam(countries, userTeam) {
 export function setCrickScore(state, action, team) {
   if (typeof action.payload === "string") {
     if (action.payload === "Wide" || action.payload === "NB") {
-      console.log("Ekumcheck extra", state[team].scorePerBall)
+      console.log("Ekumcheck extra", state[team].scorePerBall);
       return {
         ...state,
         [team]: {
           ...state[team],
-          score: state[team].score++,
-          scorePerBall: state[team].scorePerBall.push(action.payload)
+          score: state[team].score + 1,
+          scorePerBall: [...state[team].scorePerBall, action.payload],
         },
       };
     } else {
-      console.log("Ekumcheck wicket", state[team].scorePerBall)
+      console.log("Ekumcheck wicket", state[team].scorePerBall);
       return {
         ...state,
         [team]: {
           ...state[team],
-          wicket: state[team].wicket++,
-          balls:state[team].balls++,
-          scorePerBall: state[team].scorePerBall.push(action.payload)
+          wicket: state[team].wicket + 1,
+          balls: state[team].balls + 1,
+          scorePerBall: [...state[team].scorePerBall, action.payload],
         },
       };
     }
   } else {
-    console.log("Ekumcheck score", state[team].scorePerBall)
+    console.log("Ekumcheck score", state[team].scorePerBall);
     return {
       ...state,
       [team]: {
         ...state[team],
         score: state[team].score + action.payload,
-        balls:state[team].balls++,
-        scorePerBall: state[team].scorePerBall.push(action.payload)
+        balls: state[team].balls + 1,
+        scorePerBall: [...state[team].scorePerBall, action.payload],
       },
     };
   }
 }
 
-export function setInningsEnd(state, action){
-  console.log("Ekumcheck", state, action)
-  return {...state, inning:action.payload.inn, batting:action.payload.team, target:action.payload.target }
+export function setInningsEnd(state, action) {
+  console.log("Ekumcheck", state, action);
+  return {
+    ...state,
+    inning: action.payload.inn,
+    batting: action.payload.team,
+    target: action.payload.target,
+  };
 }
 
 export function ballsToOvers(balls) {
   const overs = Math.floor(balls / 6);
   const remainingBalls = balls % 6;
   return `${overs}.${remainingBalls}`;
+}
+
+export function decideBattingBowlingTeams(state) {
+  console.log(state);
+  let battingTeam;
+  let battingKey;
+  let ballingTeam;
+  let ballingKey;
+
+  if (state.batting === state.team1.name) {
+    battingTeam = state.team1.name;
+    battingKey = "team1";
+    ballingTeam = state.team2.name;
+    ballingKey = "team2";
+  } else {
+    battingTeam = state.team2.name;
+    battingKey = "team2";
+    ballingTeam = state.team1.name;
+    ballingKey = "team1";
+  }
+  console.log("Ekumcheck team batting", battingTeam, battingKey);
+  console.log("Ekumcheck team batting", ballingTeam, ballingKey);
+  return [battingKey, ballingTeam, ballingKey];
+}
+
+export function endFirstInning(state, battingKey, ballingTeam, dispatch) {
+  if (
+    state[battingKey].balls < state.overs * 6 &&
+    state[battingKey].wicket < 11
+  ) {
+    console.log("Ekumcheck Its Inning 1 Continue");
+  } else {
+    console.log("Ekumcheck Its Inning 1 End");
+    dispatch({
+      type: "SET_INNINGS_END",
+      payload: {
+        inn: 2,
+        team: ballingTeam,
+        target: state[battingKey].score + 1,
+      },
+    });
+  }
+}
+
+export function endSecondInning(state, battingKey, ballingKey, dispatch) {
+  if (state[battingKey].score < state.target) {
+    if (
+      state[battingKey].balls < state.overs * 6 &&
+      state[battingKey].wicket < 11
+    ) {
+      console.log("All Good 2nd inning can conitinue");
+    } else {
+      dispatch({ type: "SET_WINNER", payload: state[ballingKey] });
+    }
+  } else {
+    console.log("Ekumcheck 2", state.target, state[battingKey].score);
+    dispatch({ type: "SET_WINNER", payload: state[battingKey] });
+  }
+}
+
+export function randomScoreArr(){
+    const outcomes = [ 4, 2, 2, 0, 4, "Bowled", 1, "Wide", 6, 1, 1, 2, 6, 0, 3, 1, 6, "NB", 0, 1, 4, 4, 1, 1, "LBW", 2, 4, "Bowled", 2, 4, 3, 1, 0, "Wide", 2, 2, 1, 4, 0, 4, 2, "NB", 1, 6, 6, 3, 1, 0, 6, 2, 1, 4, 2, "NB", 6, 6, 4, 1, 1, 4, 3, 0, 2, 1, "Wide", 4, 6, 0, 0, 6, 2, 2, 0, 1, 2, "NB", 4, 4, 4, 6, 2, 3, "Wide", 0, 6, 6, 4, 4, "LBW", 1, 4, 2, "Bowled", 6, 1, "Wide", "NB", 2, 1, 0, 3, 4, 6, 0, "Bowled", 2, 1, 0, 2, 4, "Wide", 1, "NB", 4, 3, 6, 0, 1, 6, 0, 2, 0, 1, 2, "Wide", 2, 3, "NB", 6, 0, 4, 2, 1, 1, 0, 0, 2, "Bowled", 1, 2, 4, 3, "Wide", "NB", 1, 0, 2, 6, 1, 2, 6, 1, 6, 0, 3, "Wide", "NB", 1, 2, 4, 6, 0, 2, 4, 3, 6, 1, 2, "Wide", "NB", "LBW", 3, 4, 0, 6, 1, 6, 2, 4, 0, 1, "Wide", "NB", 3, 4, 2, 6, 6, 2, "Bowled", 1, 0, 4, "Wide", "NB", 2, 4, 1, 0, 3, 6, 4, 6, 2, "LBW", 1, ];
+    return outcomes;
 }
